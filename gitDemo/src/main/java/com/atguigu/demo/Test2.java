a7065420dba721cea7ccbef90b33aa2eeea05129
SupHansen <70797795+SupHansen@users.noreply.github.com>
2020-09-07 19:07:00 +0800

    Update Test2.java

--- gitDemo/src/main/java/com/atguigu/demo/Test2.java
+++ gitDemo/src/main/java/com/atguigu/demo/Test2.java
@@ -3,5 +3,6 @@ package com.atguigu.demo;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("第二个git文件");
+        System.out.println("此修改来自git远程库");
     }
 }
