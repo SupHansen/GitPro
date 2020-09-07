d1d90bd8ea7a006a9ae6aad8cb556179ecf42c39
hansen <597359085@qq.com>
2020-09-07 18:55:58 +0800

    commit---first

--- gitDemo/src/main/java/com/atguigu/demo/Test1.java
+++ gitDemo/src/main/java/com/atguigu/demo/Test1.java
@@ -3,5 +3,6 @@ package com.atguigu.demo;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("git学习：与远程github连接");
+        System.out.println("修改第一次");
     }
 }
---
+++ gitDemo/src/main/java/com/atguigu/demo/Test2.java
@@ -0,0 +1,7 @@
+package com.atguigu.demo;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("第二个git文件");
+    }
+}
