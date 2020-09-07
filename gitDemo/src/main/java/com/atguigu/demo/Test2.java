0e2d6a13bab1b95ab506579e605ee7b7fe68a6d2
hansen <597359085@qq.com>
2020-09-07 19:10:07 +0800

    commit---second

--- gitDemo/src/main/java/com/atguigu/demo/Test2.java
+++ gitDemo/src/main/java/com/atguigu/demo/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("第二个git文件");
         System.out.println("此修改来自git远程库");
+        System.out.println("第二次向远程库提交，产生新版本");
     }
 }
